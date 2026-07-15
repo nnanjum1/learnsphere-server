d81e8f9bc81656a2a8e8abf61dbf1127d7c42004
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-07-15 18:07:53 +0600

    better auth url added

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -5,6 +5,13 @@ import { jwt } from "better-auth/plugins";
 import { client, db } from "./db.js";
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
+
+    trustedOrigins: [
+        "http://localhost:3000",
+        "https://learnsphere-client.vercel.app",
+    ],
+
     database: mongodbAdapter(db, {
         client,
     }),
