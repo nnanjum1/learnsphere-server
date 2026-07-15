e12a3c2e24d8bfbf1b59ff949078892120a8ecdb
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-07-15 16:51:07 +0600

    Remove node_modules and dist from repository

--- dist/config/collections.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userCollection = exports.courseCollection = void 0;
-const db_1 = require("./db");
-exports.courseCollection = db_1.db.collection("courses");
-exports.userCollection = db_1.db.collection("user");
--- dist/routes/dashboard.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dashboard_controller_1 = require("../controller/dashboard.controller");
-const enrollment_controller_1 = require("../controller/enrollment.controller");
-const router = (0, express_1.Router)();
-router.get("/instructor/:email", dashboard_controller_1.getInstructorDashboard);
-router.get("/student/:email", enrollment_controller_1.getStudentDashboard);
-exports.default = router;
